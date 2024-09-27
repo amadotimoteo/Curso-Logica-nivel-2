7bc72f6136d533fd40db56102940057600695cb8
amadotimoteo <oamadotimoteo@icloud.com>
2024-09-27 19:01:16 -0300

    Create exec-1.py

---
+++ exec-1.py
@@ -0,0 +1,10 @@
+# exercicio 1 Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B. A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados nas variáveis.
+#Armazenando os valores nas variaveis
+A = 10
+B = 20
+temp = A
+A = B
+B = temp
+# print valor
+print("valor de A:", A)
+print("valor de B:", B)
