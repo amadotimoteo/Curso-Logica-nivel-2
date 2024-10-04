86157d3a7c7c5f2fedc6532f8ee93a9d153f1ad2
amadotimoteo <oamadotimoteo@icloud.com>
2024-10-04 19:38:08 -0300

    Create exec-4.py

---
+++ exec-4.py
@@ -0,0 +1,9 @@
+ Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+# Um valor numerico
+valor = int(input("Digite um valor numérico: "))
+
+# Calcula o antecessor
+antecessor = valor - 1
+
+# Escreve o antecessor na tela
+print("O antecessor é", antecessor)
