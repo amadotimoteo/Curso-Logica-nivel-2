4732a3372f0cd228fae77d05db4016a3dfddbed0
amadotimoteo <oamadotimoteo@icloud.com>
2024-10-04 19:34:10 -0300

    Create exec-3.py

---
+++ exec-3.py
@@ -0,0 +1,13 @@
+#A1
+print(4/2+2/4)
+#A2
+print(4/2+2/4)
+#B1
+print(4/(2+2)/4)
+#B2
+print(4/2+2/4)
+#C1
+print((4+2)*2-4)
+#C2
+print(4+2*2-4)
+#Tanto B quanto C possuem valores diferentes a ordem da opoeração tanto quanto o parenteses influencia no resultado  a operação de divisão e a soma são feitas de forma diferente, levando a resultados distintos
