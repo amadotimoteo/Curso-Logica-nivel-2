19ae58f6ce7595698b9e57905a3c8c5d0861b989
amadotimoteo <oamadotimoteo@icloud.com>
2024-10-04 19:40:14 -0300

    Create exerc-5.py

---
+++ exerc-5.py
@@ -0,0 +1,9 @@
+#Dimensões do retangulo
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+# Calcular a área do retângulo
+area = base * altura
+
+# Escrever a área na tela
+print("A área do retângulo é:", area)
