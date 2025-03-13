2fd9b3fa138362663019e2d24437bd8595f259f4
amadotimoteo <oamadotimoteo@icloud.com>
2025-03-13 20:26:53 -0300

    Create exec-13.py
    Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 
    10!

---
+++ exec-13.py
@@ -0,0 +1,8 @@
+# Entrada do usuário
+valor = float(input("Digite um valor: "))
+
+# Verifica se o valor é maior que 10
+if valor > 10:
+    print("É MAIOR QUE 10!")
+else:
+    print("NÃO É MAIOR QUE 10!")
