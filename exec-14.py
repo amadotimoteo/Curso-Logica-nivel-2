c0aa136f33442ab057d63c84b0a0b3a7ba6e5d40
amadotimoteo <oamadotimoteo@icloud.com>
2025-03-13 20:29:50 -0300

    Create exec-14.py
    Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo.
    
    Explicação do Código:
    O usuário insere um número.
    O programa verifica se esse número é maior que 10.
    Se for, exibe "É MAIOR QUE 10!".
    Caso contrário, exibe "NÃO É MAIOR QUE 10!".

---
+++ exec-14.py
@@ -0,0 +1,8 @@
+# Entrada do usuário
+valor = float(input("Digite um valor: "))
+
+# Verifica se o valor é positivo ou negativo
+if valor >= 0:  # Zero é considerado positivo
+    print("O valor é POSITIVO!")
+else:
+    print("O valor é NEGATIVO!")
